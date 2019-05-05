3d5ff6f544b00a908d81bd5d8951a3d2c3db5241
Erik Neville <erikeneville@gmail.com>
2019-05-05 14:14:59 -0700

    created Google firestore function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,23 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello I'm Erik");
 });
+
+const createNotification = (notification => {
+    return admin.firestore().collection('notifications')
+        .add(notification)
+        .then(doc => console.log('notification added', doc));
+})
+
+exports.jobCreated = functions.firestore
+    .document('jobs/{jobId}')
+    .onCreate(doc => {
+
+        const job = doc.data();
+        const notification = {
+            content: 'Added a new job',
+            user: `${job.autherFirstName} ${job.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+})
--- src/components/dashboard/Options.js
+++ src/components/dashboard/Options.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Options = () => {
     return (
-        <div className="card">
-            <p className="center">Options</p>
+        <div className="section">
+            <div className="card">
+                <div className="card-content">
+                    <span className="cart-title">Notifications</span>
+                    <ul className="notifications">
+                        <li>What's Up!</li>
+                        <li>What's Up!</li>
+                        <li>What's Up!</li>
+                        <li>What's Up!</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
